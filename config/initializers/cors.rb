68b21d8302e337dc446b0a6636487ddc074a4a02
vanessuniq <vanessuniq@gmail.com>
2021-01-29 22:43:05 -0500

    configured session store for http auth with cookies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
-    # allow do
-    #   origins "https://unitedwebdev.herokuapp.com"
+    allow do
+      origins "https://eventshouse.herokuapp.com"
   
-    #   resource '*',
-    #     headers: :any,
-    #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    #     credentials: true
-    # end
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
     
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: "_house_of_events_app", domain: "eventshouse-api.herokuapp.com"
