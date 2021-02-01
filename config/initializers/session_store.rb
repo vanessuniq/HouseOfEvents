d1b0f16f9eb00fdf52155976cef1e7d8fa69f3a1
vanessuniq <vanessuniq@gmail.com>
2021-02-01 14:07:48 -0500

    refactor session store setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,8 @@
-Rails.application.config.session_store :cookie_store, key: "_house_of_events_app", domain: "eventshouse-api.herokuapp.com"
+# setting cookies env
+
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_house_of_events_app", domain: "eventshouse-api.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_house_of_events_app"
+end
+
