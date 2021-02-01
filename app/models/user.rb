6ff9bf9ebd64e510d026d03204a345faee1054de
vanessuniq <vanessuniq@gmail.com>
2021-01-31 22:50:51 -0500

    customized user model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    before_save {self.email = email.downcase}
+    has_secure_password
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    #validates :email, presence: true, uniqueness: true, length: { maximum: 75 }, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, 
+                    length: { maximum: 75 }, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    validates_presence_of :name
+    
+
+end
