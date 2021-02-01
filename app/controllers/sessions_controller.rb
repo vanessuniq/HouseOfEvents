782db573045b31d2b8fe890358a5bf332f4dfeb5
vanessuniq <vanessuniq@gmail.com>
2021-02-01 13:59:37 -0500

    set logout and auto login route and controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    # to check current user
+    include CurrentUserConcern
     # create session / login
     def create
         user = User.find_by(email: login_params[:email])
@@ -6,10 +8,26 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: {logged_in: true, user: user}, status: :created
         else
-            #render json: {status: 401}
+           
             render json: {errors: ['invalid email or password']}, status: 401
         end
     end
+
+    def logged_in
+        if @current_user
+            render json: {logged_in: true, user: @current_user}
+        else
+            render json: {logged_in: false}
+        end
+        
+    end
+    
+    # logout
+    def destroy
+        reset_session
+        render json: {logged_out: true}, status: 200
+    end
+    
     
     private
     def login_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  root to: "static#home"
- resources :sessions, only: [:create]
+ resources :sessions, only: [:create, :destroy]
+ get :logged_in, to: "sessions#logged_in"
  resources :registration, only: [:create]
 end
