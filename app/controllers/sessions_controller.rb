013e803d22811d67bf7a2bf83d9a4581e8ccf572
vanessuniq <vanessuniq@gmail.com>
2021-02-01 13:13:41 -0500

    removed responder gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,13 @@
 class SessionsController < ApplicationController
-    respond_to :json
     # create session / login
     def create
         user = User.find_by(email: login_params[:email])
         if user && user.authenticate(login_params[:password])
             session[:user_id] = user.id
-            respond_with User, json: {status: :created, logged_in: true, user: user}
+            render json: {logged_in: true, user: user}, status: :created
         else
             #render json: {status: 401}
-            respond_with status: 401
+            render json: {errors: ['invalid email or password']}, status: 401
         end
     end
     
