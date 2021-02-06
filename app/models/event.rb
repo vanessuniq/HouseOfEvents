95f9a01779cf944b0ff2b14867186fc86965c691
vanessuniq <vanessuniq@gmail.com>
2021-02-05 20:58:08 -0500

    validations for event attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
+  validates_presence_of :type, :date, :host, :user_id
+  validates :title, presence: true, uniqueness: {scope: [:date, :user_id]}
 end
