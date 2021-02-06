2e5b5458cc2dec0d3a8bc595ae5c7bb29d1b5c89
vanessuniq <vanessuniq@gmail.com>
2021-02-05 20:45:34 -0500

    emphasized validation for user password confirmation on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
     #validates :email, presence: true, uniqueness: true, length: { maximum: 75 }, format: { with: VALID_EMAIL_REGEX }
-    validates :email, presence: true, 
+    validates :email, presence: true, confirmation: true
                     length: { maximum: 75 }, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     validates_presence_of :name
+    validates_presence_of :email_confirmation
     
 
 end
