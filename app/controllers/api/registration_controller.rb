f550982b4dd5402ee3df0448d8ab0c25f4064e27
vanessuniq <vanessuniq@gmail.com>
2021-02-08 22:16:30 -0500

    namespaced the sessions and registration controllers

--- app/controllers/api/registration_controller.rb
+++ app/controllers/api/registration_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationController < ApplicationController
+class Api::RegistrationController < ApplicationController
     skip_before_action :require_login, only: [:create]
     #create an account/registration
     def create
