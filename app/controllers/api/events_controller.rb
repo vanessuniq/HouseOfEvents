52ccdc32fd715932931092702ca3a1c2b152ea88
vanessuniq <vanessuniq@gmail.com>
2021-02-11 18:57:12 -0500

    defined events action

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,3 +1,26 @@
 class Api::EventsController < ApplicationController
+    # CRUD Actions
+    
+    def index
+        
+    end
+
+    def show
+        
+    end
+
+    def create
+        
+    end
+
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    
     
 end
